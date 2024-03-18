39fef3486fdf57cc39f85e46641ec0409c8ca2da
zabokaa <sab_fem@gmx.de>
2024-03-18 19:14:58 +0100

    import view + add new path to urlpatterns

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import hello_blog
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("blog/", hello_blog, name="blog")
 ]
