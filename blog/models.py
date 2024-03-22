3e7584bb0e2fc89c0b56aa4cff532531215bc279
zabokaa <sab_fem@gmx.de>
2024-03-22 20:05:14 +0100

    add approved field and delete name + email field for Comment model

--- blog/models.py
+++ blog/models.py
@@ -25,8 +25,6 @@ class Comment(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="comments_author"
     )
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    active = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
