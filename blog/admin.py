46600ab3e23b6fd2f4e7aa185e6a1b75c5db5911
zabokaa <sab_fem@gmx.de>
2024-03-22 20:06:41 +0100

    register Comment model in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 # This will make the Post model visible on the admin page
 admin.site.register(Post)
+admin.site.register(Comment)
