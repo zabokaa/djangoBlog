7a35c5d02942611af708f853c927a472442f2542
zabokaa <sab_fem@gmx.de>
2024-03-25 14:13:49 +0100

    add fields to Post method

--- blog/models.py
+++ blog/models.py
@@ -19,6 +19,8 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    field_2 = models.CharField(default="Hola world")
+    field_3 = models.CharField(null=True)
     class Meta:
         ordering = ["created_on"]
     # dunder methods always beloow the Meta class
