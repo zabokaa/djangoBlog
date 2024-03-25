09ad1e2c0861cf3f0f456eee02a4cb0da641edfa
zabokaa <sab_fem@gmx.de>
2024-03-25 13:59:40 +0100

    change meta class Comment to oldest first

--- blog/models.py
+++ blog/models.py
@@ -40,3 +40,7 @@ class Comment(models.Model):
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
