e601cfb6fed472678b1dbac41e47ecb5cd88cadb
zabokaa <sab_fem@gmx.de>
2024-04-08 21:15:57 +0200

    add class PostAdmin @ blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
 # This will make the Post model visible on the admin page
