75d0f907d96933856cfd6f82cd54f1fb80ad15a4
zabokaa <sab_fem@gmx.de>
2024-04-08 21:16:45 +0200

    add adming.register decorater @ post/admin

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+@admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status')
@@ -12,5 +13,5 @@ class PostAdmin(SummernoteModelAdmin):
 
 # Register your models here.
 # This will make the Post model visible on the admin page
-admin.site.register(Post)
+# admin.site.register(Post)
 admin.site.register(Comment)
