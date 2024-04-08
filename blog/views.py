4609c82c81db06e4bb3530d2aa28a7684ac4bef1
zabokaa <sab_fem@gmx.de>
2024-04-08 22:43:16 +0200

    change pagination from 6 to 3

--- blog/views.py
+++ blog/views.py
@@ -13,4 +13,5 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     # template_name = "post_list.html"
     template_name = "blog/index.html"
-    paginate_by = 6 # 6 posts per page
+    # paginate_by = 6 # 6 posts per page
+    paginate_by = 3
