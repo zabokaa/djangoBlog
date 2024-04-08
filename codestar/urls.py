7ceaad02f25d587c909362182692882e91e6f157
zabokaa <sab_fem@gmx.de>
2024-04-08 21:14:19 +0200

    install summernote + add to apps

--- codestar/urls.py
+++ codestar/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path("", include("blog.urls"), name="blog-urls"),
     # path("blog/", hello_blog, name="blog" )
 ]
