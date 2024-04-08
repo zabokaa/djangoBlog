e122047729f3d638b30edd72728b43142f954cf1
zabokaa <sab_fem@gmx.de>
2024-04-08 21:15:08 +0200

    update blog app to use summernotes

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
 
 # Register your models here.
 # This will make the Post model visible on the admin page
