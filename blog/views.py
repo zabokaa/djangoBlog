79050e22a7caadbb8ff9466dcfae85a5120dc55a
zabokaa <sab_fem@gmx.de>
2024-04-08 22:12:33 +0200

    update new template_name + delete post_list.html

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,5 @@ class PostList(generic.ListView):
     # model = Post
     queryset = Post.objects.filter(status=1)
     # template_name = "post_list.html"
+    template_name = "blog/index.html"
+    paginate_by = 6 # 6 posts per page
