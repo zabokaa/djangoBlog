2a2591451823ad9fa90a55419249e4a4f1a090d3
zabokaa <sab_fem@gmx.de>
2024-04-09 16:48:19 +0200

    create post_detail path + FUNC

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 from django.views import generic
 from .models import Post
@@ -15,3 +15,26 @@ class PostList(generic.ListView):
     template_name = "blog/index.html"
     # paginate_by = 6 # 6 posts per page
     paginate_by = 3
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
