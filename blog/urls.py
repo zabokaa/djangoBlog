7484573cd2701afc294ec5540af1d27ad3f62921
zabokaa <sab_fem@gmx.de>
2024-04-09 17:03:35 +0200

    add comment to urlpatterns

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    # urlpattern slug:slug creates a url path of the domain path plus the slug value:
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
