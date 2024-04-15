01aa5668f5fe0bd916187682fa889f822c217eda
zabokaa <sab_fem@gmx.de>
2024-04-16 01:11:27 +0200

    delete about app

--- about/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import About
-from django_summernote.admin import SummernoteModelAdmin
-
-# Register your models here.
-@admin.register(About)
-class AboutAdmin(SummernoteModelAdmin):
-    summernote_fields = ('content',)
