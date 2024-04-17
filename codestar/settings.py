c9c932e6d5b29c294a00a1d6f9c8695a09eb0c89
zabokaa <sab_fem@gmx.de>
2024-04-17 22:54:45 +0200

    IM sys + add test DB

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+# for testing:
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -110,6 +112,8 @@ WSGI_APPLICATION = "codestar.wsgi.application"
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.herokuapp.com",
