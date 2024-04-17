2a76f5c986f4dafc4f44f2453ee0c1df2b530cfa
zabokaa <sab_fem@gmx.de>
2024-04-17 19:23:26 +0200

    add the delete btn to post_details + staic/js/comments.js

--- blog/urls.py
+++ blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     # path("<int:event_id>/", views.event_detail, name="event_detail"),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
-
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
