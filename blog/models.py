faf672d917bf5374f0e536aa2d6e79a337b90f6e
zabokaa <sab_fem@gmx.de>
2024-04-17 21:15:02 +0200

    update blog app + makemigrations + migrate

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # constants
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -14,6 +15,7 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
